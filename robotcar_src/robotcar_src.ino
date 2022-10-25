5343c81384e03b3f09982ae661098cddd9ccf0d5
KiSchu42 <88785814+KiSchu42@users.noreply.github.com>
2022-10-25 10:31:59 +0200

    Update robotcar_src.ino
    BETA: Fahrzeug sollte auch bei nicht direktem Sensorbeschlag auch die Geschwindigkeit halten. Bei den äußeren sollte nach 2 Sekunden (ZeitDelay1) ohne erneute Belegung irgendeines Sensors ein Fullstop ausgelöst werden.

--- robotcar_src/robotcar_src.ino
+++ robotcar_src/robotcar_src.ino
@@ -1,31 +1,34 @@
 int mainSwitch = 4;
 
 //Fotosensorik
-int sensor_RR = A0;
-int sensor_R  = A1;
-int sensor_M  = A2;
-int sensor_L  = A3;
-int sensor_LL = A4;
-int sensor_TriggerLimit = 500;
+int sensor_RR = A0;             // Pin
+int sensor_R  = A1;             // Pin
+int sensor_M  = A2;             // Pin
+int sensor_L  = A3;             // Pin
+int sensor_LL = A4;             // Pin
+int sensor_TriggerLimit = 500;  // Schaltschwelle ab wann Sensor schaltet
+int direction = 0;              // 0 = Stop, 1 = Left-Left, 2 = Left, 3 = Straight, 4 = Right, 5 = Right-Right
+long Zeitnehmer1 = 0;
+long ZeitDelay1 = 2000;         // Zeit nach Linienverlust an den äußeren Sensoren in ms
 
 //Ultraschallsensorik
-int trigger=8;
-int echo=7;
-long dauer=0;
+int trigger=8;                  // Pin
+int echo=7;                     // Pin
+long dauer=0;           
 long entfernung=0;
-long minEntfernung=100;
-long maxEntfernung=200;
+long minEntfernung=100;         // Distanz Mindestabstand in cm (Bei unterschreiten Stillstand)
+long maxEntfernung=200;         // Distanz Maximalabstand in cm (Bei überschreiten Fullspeed)
 
 //Motoren
 int speed_Left = 0;
 int speed_Right = 0;
-int fullspeed = 100;
-int speed_1 = 70;
-int speed_2 = 30;
-int pwmMotorL1 = 3;
-int pwmMotorL2 = 5;
-int pwmMotorR1 = 6;
-int pwmMotorR2 = 9;
+int fullspeed = 100;            // Geschwindigkeit für Fullspeed in %
+int speed_1 = 70;               // Erste Verzögerungsgeschwindigkeit in %
+int speed_2 = 30;               // Erste Verzögerungsgeschwindigkeit in %
+int pwmMotorL1 = 3;             // Pin
+int pwmMotorL2 = 5;             // Pin
+int pwmMotorR1 = 6;             // Pin
+int pwmMotorR2 = 9;             // Pin
 
 void setup() {
   //Motoren
@@ -40,24 +43,43 @@ void setup() {
 }
 
 void loop() {
-  if (analogRead(sensor_M) > sensor_TriggerLimit) {
+  // ** Sensor detektiert letzte Richtung
+  if (analogRead(sensor_M) > sensor_TriggerLimit) {direction = 3;} 
+  else if (analogRead(sensor_R) > sensor_TriggerLimit) {direction = 4;} 
+  else if (analogRead(sensor_L) > sensor_TriggerLimit) {direction = 2;} 
+  else if (analogRead(sensor_RR) > sensor_TriggerLimit) {direction = 5;} 
+  else if (analogRead(sensor_LL) > sensor_TriggerLimit) {direction = 1;}
+
+  // ** Setze Achsgeschwindigkeiten
+  if (direction == 3) {
     speed_Left = fullspeed;
     speed_Right = fullspeed;
-  } else if (analogRead(sensor_R) > sensor_TriggerLimit) {
+  } else if (direction == 4) {
     speed_Left = fullspeed;
     speed_Right = speed_1;
-  } else if (analogRead(sensor_L) > sensor_TriggerLimit) {
+  } else if (direction == 2) {
     speed_Left = speed_1;
     speed_Right = fullspeed;
-  } else if (analogRead(sensor_RR) > sensor_TriggerLimit) {
+  } else if (direction == 5) {
     speed_Left = fullspeed;
     speed_Right = speed_2;
-  } else if (analogRead(sensor_LL) > sensor_TriggerLimit) {
+    //Wenn länger als X kein Sensor mehr getriggert wird dann STOP.
+    Zeitnehmer1 = millis();    
+    if (millis() > ZeitDelay1 + Zeitnehmer1 ) {
+      direction = 0;
+    }
+  } else if (direction == 1) {
     speed_Left = speed_2;
     speed_Right = fullspeed;
+    //Wenn länger als X kein Sensor mehr getriggert wird dann STOP.
+    Zeitnehmer1 = millis();    
+    if (millis() > ZeitDelay1 + Zeitnehmer1 ) {
+      direction = 0;
+    }
   }
+
   
-  //Ultraschallsensor Abstand
+  // ** Ultraschallsensor Abstand
   digitalWrite(trigger, LOW);
   delay(5);
   digitalWrite(trigger, HIGH);
@@ -66,7 +88,7 @@ void loop() {
   dauer = pulseIn(echo, HIGH);
   entfernung = (dauer/2) * 0.03432;
   
-  //Differenzrechnung bis Stillstan
+  // ** Differenzrechnung bis Stillstand
   if (entfernung <= maxEntfernung && entfernung >= minEntfernung)
   {
     speed_Right = speed_Right - (1.0 / (maxEntfernung - minEntfernung) * (entfernung - minEntfernung));
@@ -79,15 +101,21 @@ void loop() {
     speed_Left = 0;
   }
   
-  // Speedfaktor
+  // ** Speedfaktor
   speed_Right = speed_Right * 2,55;
   speed_Left = speed_Left * 2,55;
+
+  // ** Fullstop
+  if (direction == 0) {
+    speed_Right = 0;
+    speed_Left = 0;
+  }
   
-  // Analogwerte schreiben
+  // ** Analogwerte schreiben
   analogWrite(pwmMotorL1, speed_Right);
   analogWrite(pwmMotorL2, speed_Right);
   analogWrite(pwmMotorR1, speed_Left);
   analogWrite(pwmMotorR2, speed_Left);
     
-  delay(200);
+  delay(200); //Wegen der Simulation
 }
